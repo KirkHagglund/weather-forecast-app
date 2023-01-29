4d1f54768fac202d807ea3c0b78e940bb22c0209
elkirko <kirkhagglund@gmail.com>
2023-01-29 10:53:38 -0600

    adding js script for api fetch

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,29 @@ Requires an addEventListener
 Fetched information must be then stored in localStorage and once replaced, it should populate inside the #past-searches section
 
 */
+let cityName = document.getElementById('city');
+let currentCity = document.getElementById('current-section');
 
-let cityName = document.getElementById('city').value;
-let requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=3f84325a852afcf6282b9ff4cc366d95';
-fetch(requestUrl)
-    .then(function (response) {
-        return respsonse.json();
+let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=3f84325a852afcf6282b9ff4cc366d95';
+
+var submitRequest = function (event) {
+    event.preventDefault();
+    
+    var cityInput = cityName.ariaValueMax.trim();
+
+    if (cityInput) {
+        getCityWeather(cityInput);
+
+        currentCity.textContent = '';
+        cityName.value = '';
+    }   else {
+        alert('PLease enter a city name');
+    }
+};
+fetch(requestUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response);        }
+            respsonse.json().then(function (data) {
+                console.log(data);               
+            });
     });
