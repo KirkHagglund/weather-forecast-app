ee61dacdc729caa027cc57e1b622241078581e58
elkirko <kirkhagglund@gmail.com>
2023-02-01 08:50:11 -0600

    Trying to get the past buttons to call fetch functions

--- assets/script.js
+++ assets/script.js
@@ -26,12 +26,12 @@ var submitRequest = function (event) {
 
 var recallRequest = function (event) {
     event.preventDefault();
+    forecastSection.textContent = "";
+    currentCity.textContent = "";
+    var cityInput = //WHAT IS THIS
 
-    var recallInput = pastCard.button;
-    console.log(recallInput);
-
-   /* getCityWeather(recallInput);
-    getForecast(recallInput);*/
+    recallForecast(cityInput);
+    recallWeather(cityInput);
 }
 
 //Current weather function
@@ -122,20 +122,96 @@ const getForecast = (city) => {
     });
 };
 
+const recallWeather = (/*THIS GOES HERE*/) => {
+    const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=3f84325a852afcf6282b9ff4cc366d95&units=metric';
+    fetch(requestUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+         };
+            response.json().then(function (data) {
+                console.log(data); 
+                console.log(data.name, data.weather[0].icon, data.main.temp);
+                let cityHeading = document.createElement('h3');
+                let cityWeather = document.createElement('img');
+                let cityTemp = document.createElement('p');
+                let cityHumidity = document.createElement('p');
+                let cityWind = document.createElement('p');
+                
+                cityHeading.textContent = data.name + ' ' + today.format('MMM D, YYYY');
+                cityTemp.textContent = 'Temp: ' + Math.floor(data.main.temp) + '°C';
+                cityHumidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+                cityWind.textContent = 'Wind: ' + data.wind.speed + ' KmPH';
+                cityWeather.src = 'http://openweathermap.org/img/wn/' + (data.weather[0].icon) + '@2x.png';                            
+                
+                currentCity.appendChild(cityHeading);
+                currentCity.appendChild(cityWeather);
+                currentCity.appendChild(cityTemp);
+                currentCity.appendChild(cityHumidity);
+                currentCity.appendChild(cityWind);
+            });
+    });
+};
+
+const recallForecast = (/*THIS GOES HERE*/) => {
+    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=3f84325a852afcf6282b9ff4cc366d95&units=metric';
+    fetch(forecastUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+        };
+            response.json().then(function (data) {
+                console.log(data);
+                let forecastArray = [data.list[7], data.list[15], data.list[23], data.list[31], data.list[39]];
+                console.log(forecastArray);
+
+                for (i=0; i<forecastArray.length; i++) {
+                    let forecastHeading = document.createElement('h4');
+                    let forecastWeather = document.createElement('img');
+                    let forecastTemp = document.createElement('p');
+                    let forecastHumidity = document.createElement('p');
+                    let forecastCard = document.createElement('div');
+                    let unixTimestamp = forecastArray[i].dt;
+                    let milliseconds = unixTimestamp * 1000;
+                    let dateObject = new Date(milliseconds);
+                    let humanDayFormat = dateObject.toLocaleString('en-us', {weekday: 'short'});
+                    let humanDateFormat = dateObject.toLocaleString('en-us', {day: 'numeric'});
+                    let humanMonthFormat = dateObject.toLocaleString('en-us', {month: 'short'});
+                    forecastCard.style.width = '19%';
+                    forecastCard.style.height = 'fit-content';
+                    forecastCard.style.backgroundColor = 'rgb(24, 24, 143)';
+                    forecastCard.style.color = 'rgb(192, 205, 241)';
+                    forecastCard.style.margin = '3px';
+                    forecastWeather.style.height = '75px';
+                    forecastHeading.textContent = humanDayFormat + ' ' + humanDateFormat + ' ' + humanMonthFormat;
+                    forecastTemp.textContent = 'Temp: ' + Math.floor(forecastArray[i].main.temp) + '°C';
+                    forecastHumidity.textContent = 'Humidity: ' + forecastArray[i].main.humidity + '%';
+                    forecastWeather.src = 'http://openweathermap.org/img/wn/' + (forecastArray[i].weather[0].icon) + '@2x.png';
+
+                    forecastSection.appendChild(forecastCard);
+                    forecastCard.appendChild(forecastHeading);
+                    forecastCard.appendChild(forecastWeather);
+                    forecastCard.appendChild(forecastTemp);
+                    forecastCard.appendChild(forecastHumidity);
+                };          
+            });
+    });
+};
+
 const getLocalStorage = function() {
-    //JSON.parse(localStorage.getItem('cities'));
     for (i = 0; i < localStorageArr.length; i++) {
         const button = document.createElement("button");
                 button.classList.add("past-search-btn");
                 button.textContent = localStorageArr[i];
                 button.setAttribute('class', 'search-card');
+                button.addEventListener("click", function(event){
+                    event.preventDefault();
+                    console.log(event.target.textContent);
+                    recallRequest();
+                })
                 pastCard.appendChild(button);
     };
 };
 
 //Event listener to trigger fetch calls
 searchBtn.addEventListener('click', submitRequest);
-
-pastCard.addEventListener('click', recallRequest);
 
 getLocalStorage();
